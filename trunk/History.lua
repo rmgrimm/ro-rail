8fdcf1b0a474fbf2bd893e3e938e669c3ad29f8f
faithful613 <faithful613@73d313aa-8140-11dd-b774-4f160e1b508b>
2008-10-11 09:34:36 +0000

    Moved history tracking class from Actor.lua to History.lua. Implemented history-aware Utils.lua wrappers, and examples.

---
+++ trunk/History.lua
@@ -0,0 +1,141 @@
+do
+	-- History tracking
+	History = {}
+
+
+	-- Some 'private' keys for our history table
+	local default_key = {}
+	local list_key = {}
+	local subtimes_key = {}
+	local different_key = {}
+
+	-- A helper funvtion to calculate values
+	History.SubValue = function(a,b,target)
+		-- Calculate the time difference ratio of A->B to A->Target
+		local dest_ratio = (b[2] - a[2]) / (target - a[2])
+
+		-- Divide the A->B value difference by the ratio,
+		--	then apply it back to A to get the target value
+		return a[1] + (b[1] - a[1]) / dest_ratio
+	end
+
+	local History_mt = {
+		__index = function(self,key)
+			local list = self[list_key]
+
+			-- Check if we have any history
+			if list:Size() < 1 then
+				-- No history, return the default
+				return self[default_key]
+			end
+
+			-- How many milliseconds into the past?
+			--	negative indexes will "predict" future values
+			local target = GetTick() - key
+
+			-- If time older than history is requested, use default
+			if target < list[list.first][2] then
+				return self[default_key]
+			end
+
+			-- If time more recent than latest history, use it
+			if target >= list[list.last][2] then
+				if list:Size() < 2 then
+					-- Since size is only 1, we can't calculate
+					return list[list.last][1]
+				end
+
+				return History.SubValue(list[list.last-1],list[list.last],target)
+			end
+
+			-- Otherwise, binary search for the closest item that isn't newer
+			do
+				-- left is older, right is newer
+				local l,r = list.first,list.last
+				local probe
+				while true do
+					probe = math.floor((l + r) / 2)
+
+					if probe <= l then
+						-- This must be the best one
+						break
+					end
+
+					-- Check the time of the current probe position
+					if target < list[probe][2] then
+						-- Too new
+						r = probe
+					elseif target > list[probe][2] then
+						-- New enough, search for a better one
+						l = probe
+					else
+						-- If it's exact, go ahead and use it
+						return list[probe][1]
+					end
+				end
+
+				if not self[subtimes_key] then
+					return list[probe][1]
+				end
+
+				return History.SubValue(list[probe],list[probe+1],target)
+			end
+		end,
+		__newindex = function(self,key,val)
+			-- Don't allow new entries to be created directly
+		end,
+
+		-- For consistency with Util wrappers below
+		__call = function(self,key)
+			return self[key]
+		end,
+	}
+
+	local default_Different = function(a,b) return a[1] ~= b[1] end
+
+	History.New = function(default_value,calc_sub_vals,diff_func)
+		local ret = {
+			[default_key] = default_value,
+			[list_key] = List.New(),
+			[subtimes_key] = calc_sub_vals,
+			[different_key] = default_Different
+		}
+		setmetatable(ret,History_mt)
+
+		if type(diff_func) == "function" then
+			ret[different_key] = diff_func
+		end
+
+		return ret
+	end
+
+	History.Update = function(table,value)
+		local list = table[list_key]
+		local diff = table[different_key]
+
+		-- New value
+		value = {value,GetTick()}
+
+		-- Make sure it's not a duplicate
+		if list:Size() < 1 or diff(list[list.last],value) then
+			list:PushRight(value)
+			return
+		end
+
+		-- If we don't calculate sub-values, it won't matter
+		if not table[subtimes_key] then return end
+
+		-- Since sub-values are calculated, keep the beginning and end times
+		if list:Size() < 2 or diff(list[list.last-1],list[list.last]) then
+			list:PushRight(value)
+			return
+		end
+
+		-- If there's already beginning and end, update the end
+		list[list.last] = value
+	end
+
+	History.Clear = function(table)
+		table[list_key]:Clear()
+	end
+end
