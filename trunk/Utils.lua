cad02ec63cb63e658c90a08dda8e18d74cb1cab1
faithful613 <faithful613@73d313aa-8140-11dd-b774-4f160e1b508b>
2008-10-26 06:03:17 +0000

    Added CompareAngle() function

--- trunk/Utils.lua
+++ trunk/Utils.lua
@@ -57,5 +57,70 @@ do
 		return x+x_delta, y+y_delta
 	end
 
-	-- TODO: Angle compare
+	-- Check to see if an angle is within X radians of a second
+	function CompareAngle(angle1,angle2,radians)
+		local bottom,top
+	
+		-- Get the modulus remainder of both angles
+		angle1 = math.mod(angle1,360)
+		angle2 = math.mod(angle2,360)
+	
+		-- Check if radians is positive or negative
+		if radians > 0 then
+			-- Check if adding radians to angle1 would throw it past 359
+			if angle1 + radians >= 360 then
+				-- Make a second check
+				if CompareAngle(0,angle2,math.mod(angle1 + radians,360)) then
+					-- Found it already
+					return true
+				end
+	
+				-- Reduce radians below 360
+				radians = radians - math.mod(angle1 + radians,360) - 1
+			end
+	
+			-- Set the bottom and the top
+			bottom = angle1
+			top = angle1 + radians
+	
+		elseif radians < 0 then
+			-- Invert the radians, to make later stuff easier to read
+			radians = -radians
+	
+			-- Check if subtracting radians would throw it under 0
+			if angle1 - radians < 0 then
+				-- Make a second check
+				if CompareAngle(360 + angle1 - radians,angle2,math.abs(angle1 - radians) - 1) then
+					-- Found it already
+					return true
+				end
+	
+				-- Increase the radians above -1
+				radians = radians + math.abs(angle1 - radians)
+			end
+	
+			-- Set the bottom
+			bottom = angle1 - radians
+			top = angle1
+	
+		else
+			-- If radians is 0, then check if the angles are the same
+			if angle1 == angle2 then
+				-- They check out as the same
+				return true
+			end
+	
+			-- They're not the same
+			return false
+		end
+	
+		-- Check if its above the bottom but below the top, inclusively
+		if bottom <= angle2 and angle2 <= top then
+			-- Its inside
+			return true
+		end
+	
+		-- Its not inside
+		return false
+	end
 end
