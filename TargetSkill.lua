a2b36e7b7023dc051f8f5add5cba105a72a72ee2
faithful613 <faithful613@73d313aa-8140-11dd-b774-4f160e1b508b>
2010-11-27 03:27:58 +0000

    Implement AI for PartySupport skills (Mercenary's Sacrifice)

--- TargetSkill.lua
+++ TargetSkill.lua
@@ -31,7 +31,7 @@ RAIL.Event["AI CYCLE"]:Register(-35,                    -- Priority
     --Emergency     = "SKILL INIT/EMERGENCY",
     --HealOwner     = "SKILL INIT/HEAL OWNER",
     --HealSelf      = "SKILL INIT/HEAL SELF",
-    --PartySupport  = "SKILL INIT/PARTY BUFF",
+    PartySupport  = "SKILL INIT/PARTY BUFF",
     Provoke       = "SKILL INIT/PROVOKE",       -- also fires SKILL INIT/OFFENSIVE
     --Pushback      = "SKILL INIT/PUSHBACK",
     --Recover       = "SKILL INIT/RECOVER",
@@ -115,7 +115,8 @@ do
                                   -1,                     -- Max runs (negative means infinite)
                                   function()              -- Handler function
     if RAIL.SkillState == RAIL.SkillState.CASTING then
-      RAIL.LogT(7,"Casting motion prevents action; cycle terminating after data collection.")
+      -- Too spammy
+      --RAIL.LogT(7,"Casting motion prevents action; cycle terminating after data collection.")
 
       -- Discontinue this AI CYCLE
       return false
@@ -157,31 +158,56 @@ do
       return false
     end
 
-    -- Check for heal skills first
+    -- Check for party support skills
+    FireSkillEvent("TARGET SELECT/SKILL/PARTY BUFF", "PartySupport", 0)
+
+    -- Check for heal skills
     FireSkillEvent("TARGET SELECT/SKILL/HEAL OWNER", "HealOwner",  0)
     FireSkillEvent("TARGET SELECT/SKILL/HEAL SELF",  "HealSelf",   0)
   end)
 
-  -- Select the skill
-  RAIL.Event["TARGET SELECT/POST"]:Register(10,               -- Priority
-                                            "Select skill",   -- Handler name
-                                            -1,               -- Max runs (negative means infinite)
+  -- Check buff skills
+  RAIL.Event["TARGET SELECT/POST"]:Register(10,                   -- Priority
+                                            "Buff skill select",  -- Handler name
+                                            -1,                   -- Max runs (negative means infinite)
                                             function()
     -- Check if skills are ready
     if RAIL.SkillState ~= RAIL.SkillState.READY then
       -- Skills aren't ready
-      return false
+      return
     end
 
     -- Check if a skill was manually requested
     if RAIL.Target.Skill and RAIL.Target.Skill.Manual then
       -- Don't try a new skill
-      return false
+      return
     end
 
     -- Check buff skills
     -- NOTE: Some buff skill conditions require knowledge of decided movement
     FireSkillEvent("TARGET SELECT/SKILL/BUFF","Buff")
+  end)
+
+  -- Check that the AI won't initiate a cast-time skill while chasing
+  RAIL.Event["TARGET SELECT/POST"]:Register(50,               -- Priority
+                                            "Walk+cast time", -- Handler name
+                                            -1,
+                                            function()
+    -- Check that a skill was selected
+    if not RAIL.Target.Skill then
+      return
+    end
+    
+    -- Check that a chase location was selected
+    if not RAIL.Target.Chase then
+      return
+    end
+    
+    -- Check if the skill has a cast time
+    if RAIL.Target.Skill[1].CastTime > 0 then
+      -- Remove the skill
+      RAIL.Target.Skill = nil
+    end
   end)
 
   RAIL.Event["IDLE"]:Register(20,                   -- Priority
@@ -247,7 +273,7 @@ do
 
     -- Set the next time that the skill should be cast
     if skill.Duration > 0 then
-      target.BattleOpts[skill.ID .. "next"] = GetTick() - ticks + skill.Duration
+      target.BattleOpts[skill.ID .. "next"] = GetTick() - ticks + skill.Duration - skill.CastTime
     end
   end
 
@@ -403,7 +429,7 @@ RAIL.Event["TARGET SELECT/ENEMY/SKILL/OFFENSIVE"]:Register(40,              -- P
                                                            -1,              -- Max runs (infinite)
                                                            function(self,skill,actor,can_chase)
   -- Get the skill range
-  local srange = skill:GetRange(level)
+  local srange = skill:GetRange()
 
   -- If the actor was allowed it can be chased
   if can_chase then
@@ -617,7 +643,7 @@ do
     failures[skill.ID] = 0
 
     -- Set the next time we can use the buff
-    RAIL.State.SkillOptions[skill.ID].NextCastTime = GetTick() + skill.Duration - ticks
+    RAIL.State.SkillOptions[skill.ID].NextCastTime = GetTick() - ticks + skill.Duration - skill.CastTime
   end
 
   local function FailureCallback(ticks,skill)
@@ -675,61 +701,334 @@ do
       return false
     end
   end)
+end
 
-  RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(10,             -- Priority
-                                                  "Next cast",    -- Handler name
-                                                  -1,             -- Max runs
-                                                  function(self,skill,idleticks)
-    -- Get the time that the buff will wear off
-    local next_cast = RAIL.State.SkillOptions[skill.ID].NextCastTime
+RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(10,             -- Priority
+                                                "Next cast",    -- Handler name
+                                                -1,             -- Max runs
+                                                function(self,skill,idleticks)
+  -- Get the time that the buff will wear off
+  local next_cast = RAIL.State.SkillOptions[skill.ID].NextCastTime
 
-    -- Don't use the buff if it's still active
-    if GetTick() < next_cast then
-      return false
+  -- Don't use the buff if it's still active
+  if GetTick() < next_cast then
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(20,             -- Priority
+                                                "Condition",    -- Handler name
+                                                -1,             -- Max runs
+                                                function(self,skill,idleticks)
+  -- Check any custom condition
+  if not RAIL.State.SkillOptions[skill.ID].Condition(RAIL._G,nil) then
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(50,             -- Priority
+                                                "Acceptable",   -- Handler name
+                                                -1,             -- Max runs
+                                                function(self,skill,idleticks)
+  -- If there's not a selected skill, use this one
+  if not RAIL.Target.Skill then
+    RAIL.Target.Skill = { skill, RAIL.Self }
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(60,             -- Priority
+                                                "Priority",     -- Handler name
+                                                -1,             -- Max runs
+                                                function(self,skill,idleticks)
+  -- Get the priority levels
+  local new_prio = skill:GetPriority()
+  local old_prio = RAIL.Target.Skill[1]:GetPriority(RAIL.Target.Skill[2],   -- X or actor
+                                                    RAIL.Target.Skill[3])   -- Y
+  -- Check if this skill is lower priority
+  if new_prio < old_prio then
+    -- Interrupt this event
+    return false
+  end
+  
+  -- Check if this skill is highest priority
+  if new_prio > old_prio then
+    -- Set the skill to this one and then interrupt the event
+    RAIL.Target.Skill = { skill, RAIL.Self }
+    return false
+  end
+end)
+
+-----------------
+-- Party Buffs --
+-----------------
+
+do
+  -- State validation options
+  local defaults = {is_subtable = true,
+    MaxFailures = {"number",10,1},
+    PriorityOffset = {"number",0},
+    AutoIncludeOwner = {"boolean",true},
+  }
+
+  -- Table of targets for each skill
+  local targets = {}
+  
+  -- Number of failures by skill ID
+  local failures = {}
+  
+  -- Boolean to set when a party support buff is initialized
+  local have_partybuff = false
+
+  -- Function to get the buff priority
+  local function GetPriority(self,actor)
+    local base_prio
+    if actor.BattleOpts.Priority == RAIL.State.ActorOptions.Default.Priority then
+      base_prio = RAIL.State.SkillOptions.BuffBasePriority
+    else
+      base_prio = actor.BattleOpts.Priority
     end
-  end)
+    local prio_offset = RAIL.State.SkillOptions[self.ID].PriorityOffset
+    return base_prio + prio_offset
+  end
 
-  RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(20,             -- Priority
-                                                  "Condition",    -- Handler name
-                                                  -1,             -- Max runs
-                                                  function(self,skill,idleticks)
-    -- Check any custom condition
-    if not RAIL.State.SkillOptions[skill.ID].Condition(RAIL._G,nil) then
-      return false
+  local function SuccessCallback(ticks,skill,target)
+    -- Reset the failure count
+    failures[skill.ID] = 0
+
+    -- Set the next time we can use the buff
+    target.BattleOpts[skill.ID .. "next"] = GetTick() - ticks + skill.Duration - skill.CastTime
+  end
+
+  local function FailureCallback(ticks,skill,target)
+    -- Increment the failure count if the skill hasn't been confirmed
+    failures[skill.ID] = failures[skill.ID] + 1
+  end
+
+  -- Initialization
+  RAIL.Event["SKILL INIT/PARTY BUFF"]:Register(0,         -- Priority
+                                               "Init",    -- Handler name
+                                               -1,        -- Max runs (infinite)
+                                               function(self,skill)
+    local byID = RAIL.Validate.SkillOptions
+
+    -- Copy validation options from defaults, but don't overwrite
+    byID[skill.ID] = Table.DeepCopy(defaults,byID[skill.ID],false)
+    
+    -- Create the targets list
+    targets[skill.ID] = List.New()
+    
+    -- Check if we should add the owner right away
+    if RAIL.State.SkillOptions[skill.ID].AutoIncludeOwner then
+      targets[skill.ID]:PushRight(RAIL.Owner)
+    end
+
+    -- Set initial failure count
+    failures[skill.ID] = 0
+    
+    -- Set that a party buff exists
+    have_partybuff = true
+
+    -- Add callbacks to the skill
+    RAIL.SkillState.Callbacks:Add(skill,            -- Skill to add callbacks to
+                                  SuccessCallback,
+                                  FailureCallback,
+                                  true)             -- Persist past the first call
+
+
+    -- Add the GetPriority support function
+    if not skill[1] then
+      -- Skill level not selectable
+      skill.GetPriority = GetPriority
+    else
+      -- Skill level selectable
+      local i=1
+      while skill[i] do
+        skill[i].GetPriority = GetPriority
+        i = i + 1
+      end
     end
   end)
   
-  RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(50,             -- Priority
-                                                  "Acceptable",   -- Handler name
-                                                  -1,             -- Max runs
-                                                  function(self,skill,idleticks)
-    -- If there's not a selected skill, use this one
+  -- Check for new target
+  RAIL.Event["AI CYCLE"]:Register(915,                        -- Priority
+                                  "New party-buff target?",   -- Handler name
+                                  -1,                         -- Max runs
+                                  function(self)
+    -- Ensure that there are party buffs to check for
+    if not have_partybuff then
+      -- Never run this handler again
+      self.RunsLeft = 0
+      return
+    end
+
+    -- Check that there is a selected skill
     if not RAIL.Target.Skill then
-      RAIL.Target.Skill = { skill, RAIL.Self }
+      return
+    end
+    
+    -- Check that the selected skill is manually selected
+    if not RAIL.Target.Skill.Manual then
+      return
+    end
+    
+    -- Check that the skill is a party buff skill
+    local list = targets[RAIL.Target.Skill[1].ID]
+    if not list then
+      return
+    end
+    
+    -- Get the target of the skill
+    local target = RAIL.Target.Skill[2]
+
+    -- Check if the target of this skill is in the targets list
+    for i=list.first,list.last do
+      if list[i] == target then
+        -- Don't add this target since its already in the list
+        return
+      end
+    end
+    
+    -- Log that a new target was added
+    RAIL.LogT(60,"New party-support target for {1}: {2}",RAIL.Target.Skill[1],target)
+
+    -- Add this target to the list
+    list:PushRight(target)
+
+    -- Ensure the list isn't too big
+    -- TODO: Incorporate a different way to specify the max number of
+    -- simultaneous targets
+    while list:Size() > RAIL.Target.Skill[1].Level do
+      -- Remove a target
+      local old_target = list:PopLeft()
+
+      -- Log the removal of this target
+      RAIL.LogT(60,"Party-support target for {1} removed: {2}",RAIL.Target.Skill[1],target)
+    end
+  end)
+  
+  -- Skill selection
+  RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF"]:Register(0,                  -- Priority
+                                                        "Failures check",   -- Handler name
+                                                        -1,                 -- Max runs
+                                                        function(self,skill,idleticks)
+    -- Check if the skill has failed too many times
+    if failures[skill.ID] >= RAIL.State.SkillOptions[skill.ID].MaxFailures then
+      -- Probably don't have the skill; stop trying
       return false
     end
   end)
 
-  RAIL.Event["TARGET SELECT/SKILL/BUFF"]:Register(60,             -- Priority
-                                                  "Priority",     -- Handler name
-                                                  -1,             -- Max runs
-                                                  function(self,skill,idleticks)
-    -- Get the priority levels
-    local new_prio = skill:GetPriority()
-    local old_prio = RAIL.Target.Skill[1]:GetPriority(RAIL.Target.Skill[2],   -- X or actor
-                                                      RAIL.Target.Skill[3])   -- Y
-    -- Check if this skill is lower priority
-    if new_prio < old_prio then
-      -- Interrupt this event
-      return false
-    end
-    
-    -- Check if this skill is highest priority
-    if new_prio > old_prio then
-      -- Set the skill to this one and then interrupt the event
-      RAIL.Target.Skill = { skill, RAIL.Self }
+  RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF"]:Register(0,                  -- Priority
+                                                        "Failures check",   -- Handler name
+                                                        -1,                 -- Max runs
+                                                        function(self,skill,idleticks)
+    -- Check if the skill has failed too many times
+    if failures[skill.ID] >= RAIL.State.SkillOptions[skill.ID].MaxFailures then
+      -- Probably don't have the skill; stop trying
       return false
     end
   end)
+
+  RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF"]:Register(10,               -- Priority
+                                                        "Per actor",      -- Handler name
+                                                        -1,               -- Max runs (infinite)
+                                                        function(self,skill,idleticks)
+    -- Loop through target actors
+    local list = targets[skill.ID]
+    local i = list.first
+    while i <= list.last do
+      -- Check if the actor is active
+      if list[i].Active then
+        -- Fire a sub event
+        RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Fire(skill,list[i])
+
+        -- Increment i
+        i = i + 1
+      else
+        -- Remove the inactive actor by shifting others forward
+        for j=i,list.last - 1 do
+          list[j] = list[j + 1]
+        end
+
+        -- Set the former last element to nil
+        list[list.last] = nil
+
+        -- Decrement list.last
+        list.last = list.last - 1
+      end
+    end
+  end)
 end
+
+RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Register(10,          -- Priority
+                                                               "Next cast", -- Handler name
+                                                               -1,          -- Max runs (infinite)
+                                                               function(self,skill,actor)
+  if GetTick() < (actor.BattleOpts[skill.ID .. "next"] or 0) then
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Register(20,          -- Priority
+                                                               "Condition", -- Handler name
+                                                               -1,          -- Max runs (infinite)
+                                                               function(self,skill,actor)
+  if not RAIL.State.SkillOptions[skill.ID].Condition(RAIL._G,actor) then
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Register(40,            -- Priority
+                                                               "Range/Chase", -- Handler name
+                                                               -1,            -- Max runs (infinite)
+                                                               function(self,skill,actor)
+  local srange = skill:GetRange()
+
+  -- Fire an event to add the target to the ChaseMap
+  RAIL.Event["TARGET SELECT/ENEMY/CHASE"]:Fire(actor,
+                                               srange,
+                                               skill:GetPriority(actor),
+                                               true)    -- srange uses PythagDistance
+
+  -- Check the range
+  if RAIL.Self:DistanceTo(actor) > srange then
+    -- Don't continue this event
+    return false
+  end
+end)
+
+
+RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Register(50,             -- Priority
+                                                               "Acceptable",   -- Handler name
+                                                               -1,             -- Max runs
+                                                               function(self,skill,actor)
+  -- If there's not a selected skill, use this one
+  if not RAIL.Target.Skill then
+    RAIL.Target.Skill = { skill, actor }
+    return false
+  end
+end)
+
+RAIL.Event["TARGET SELECT/SKILL/PARTY BUFF/BY ACTOR"]:Register(60,             -- Priority
+                                                               "Priority",     -- Handler name
+                                                               -1,             -- Max runs
+                                                               function(self,skill,actor)
+  -- Get the priority levels
+  local new_prio = skill:GetPriority(actor)
+  local old_prio = RAIL.Target.Skill[1]:GetPriority(RAIL.Target.Skill[2],   -- X or actor
+                                                    RAIL.Target.Skill[3])   -- Y
+  -- Check if this skill is lower priority
+  if new_prio < old_prio then
+    -- Interrupt this event
+    return false
+  end
+
+  -- Check if this skill is highest priority
+  if new_prio > old_prio then
+    -- Set the skill to this one and then interrupt the event
+    RAIL.Target.Skill = { skill, actor }
+    return false
+  end
+end)
 
